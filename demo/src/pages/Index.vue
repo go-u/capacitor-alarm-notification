efc641b3b693d29c3b125d45e6fd8909afce86ed
go-u <53477091+go-u@users.noreply.github.com>
2019-11-10 18:20:03 +0900

    added: notify button for iOS

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -3,12 +3,13 @@
     <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
     <q-btn :label="alarmTestTime + 'sec: vibrate'" @click="setAlarm(alarmTestTime, false)"/>
     <q-btn :label="alarmTestTime + 'sec: sound'" @click="setAlarm(alarmTestTime, true)"/>
+    <q-btn label="notify(iOS)" @click="notify()"/>
   </q-page>
 </template>
 
 <script>
 import { Plugins } from '@capacitor/core'
-const { CapacitorAlarmNotification } = Plugins
+const { CapacitorAlarmNotification, LocalNotifications } = Plugins
 
 const alarmTestTime = 3
 
@@ -34,6 +35,27 @@ export default {
         }).catch((err) => {
           console.log(err)
         })
+    },
+    notify: function () {
+      LocalNotifications.schedule({
+        notifications: [
+          {
+            title: 'Title',
+            body: 'Body',
+            id: 1,
+            schedule: { at: new Date(Date.now() + 1000 * 5) },
+            sound: './public/assets/sounds/alarm.aiff',
+            attachments: null,
+            actionTypeId: '',
+            extra: null
+          }
+        ]
+      })
+        .then((res) => {
+          console.log(res)
+        }).catch((err) => {
+        console.log(err)
+      })
     }
   }
 }
