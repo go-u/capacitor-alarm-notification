f1f5356b5a441c8025157b9bee4643ac62b2f6de
go-u <53477091+go-u@users.noreply.github.com>
2019-11-10 15:49:46 +0900

    import: CapacitorAlarmNotification plugin

--- demo/src-capacitor/android/app/src/main/java/com/demo/capacitor/alarm/notification/MainActivity.java
+++ demo/src-capacitor/android/app/src/main/java/com/demo/capacitor/alarm/notification/MainActivity.java
@@ -6,6 +6,8 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
+// my plugin
+import com.go.capacitor.alarm.notification.CapacitorAlarmNotification;
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -16,6 +18,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      add(CapacitorAlarmNotification.class);
     }});
   }
 }
