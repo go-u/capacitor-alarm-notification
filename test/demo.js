3ca8f663cbbc51128f9f33c0b5729428e7f3b100
go-u <53477091+go-u@users.noreply.github.com>
2019-11-21 18:56:57 +0900

    test run

--- test/demo.js
+++ test/demo.js
@@ -17,7 +17,6 @@ const opts = {
 
 async function main () {
   const client = await wdio.remote(opts);
-
   // const field = await client.$("android.widget.EditText");
   // await field.setValue("Hello World!");
   // const value = await field.getText();
